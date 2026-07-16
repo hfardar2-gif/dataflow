e8fee247f184710b815ffa5e473ad961ef28253e
hfardar2-gif <h.fardar2@gmail.com>
2026-07-17 00:44:19 +0330

    Add js/packets.js

---
+++ js/packets.js
@@ -0,0 +1,2 @@
+export const packetTypes=[['سفارش مشتری','sales'],['برنامه تولید','production'],['درخواست خرید','sales'],['به‌روزرسانی موجودی','warehouse'],['گزارش QC','quality'],['فاکتور','finance'],['اعلامیه حمل','warehouse'],['محاسبه هزینه','finance']];
+
